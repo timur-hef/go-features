6c7da976da7da3497eb27c57c210f3060bbbc09e
Timur Fattakhov <maseft@Timurs-MacBook-Pro.local>
2026-03-03 19:40:26 -0300

    select statement

---
+++ concurrency/select.go
@@ -0,0 +1,47 @@
+package concurrency
+
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
+
+func Racer(a, b string) (winner string) {
+	aDuration := measureResponseTime(a)
+	bDuration := measureResponseTime(b)
+
+	fmt.Printf("duration of the resource A: %s\n", aDuration)
+	fmt.Printf("duration of the resource B: %s\n", bDuration)
+
+	if aDuration < bDuration {
+		return a
+	}
+
+	return b
+}
+
+func measureResponseTime(url string) time.Duration {
+	start := time.Now()
+	http.Get(url)
+	return time.Since(start)
+}
+
+func RacerSelect(a, b string) (string, error) {
+	select {
+	case <-ping(a):
+		return a, nil
+	case <-ping(b):
+		return b, nil
+	case <-time.After(10 * time.Second):
+		return "", fmt.Errorf("timed out waiting for %s and %s", a, b)
+	}
+}
+
+func ping(url string) chan struct{} {
+	ch := make(chan struct{})
+	go func() {
+		http.Get(url)
+		close(ch)
+	}()
+	return ch
+}
